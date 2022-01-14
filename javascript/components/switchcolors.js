33778da6675e13ede1e0a73cdd7231bcdb49b059
clement moreno <clementmoreno0803@gmail.com>
2022-01-14 17:52:04 +0100

    gestion changement couleurs au clic

--- javascript/components/switchcolors.js
+++ javascript/components/switchcolors.js
@@ -1,4 +1,8 @@
     const theme = ['theme1', 'theme2', 'theme3', 'theme4', 'theme5'];
+    const nav = document.querySelector('nav')
+    const projects = document.querySelector('.projets')
+
+
     let previousNumber = -1
     document.body.addEventListener('click', () => {
         let randomNumber = Math.floor(Math.random() * 5);
@@ -14,4 +18,10 @@
             console.log(intro);
             const result = document.body.classList.add(selectedTheme);
         }
+    })
+    nav.addEventListener('click', (e) => {
+        e.stopPropagation()
+    })
+    projects.addEventListener('click', (e) => {
+        e.stopPropagation()
     })
