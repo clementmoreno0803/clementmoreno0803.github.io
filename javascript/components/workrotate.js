7ee64fae7992c0d824c6d361a450618fa9ff46dc
clement moreno <clementmoreno0803@gmail.com>
2022-01-25 17:19:03 +0100

    dernières modifs

--- javascript/components/workrotate.js
+++ javascript/components/workrotate.js
@@ -1,7 +1,7 @@
 const work = document.querySelectorAll('.asset')
 if (work) {
     window.addEventListener('scroll', () => {
-        if (window.scrollY > 1100) {
+        if (window.scrollY > 1000) {
             work.forEach((e) => {
                 e.classList.add('active')
             })
